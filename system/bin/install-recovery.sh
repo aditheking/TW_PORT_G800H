f2131dc14cad2548565b5e9ebb35b409073ac7db
aditheking <aupreti72@gmail.com>
2016-11-07 10:55:43 +0000

    Merge Indian S5 Mini Firmware

--- system/bin/install-recovery.sh
+++ system/bin/install-recovery.sh
@@ -1,6 +1,6 @@
 #!/system/bin/sh
-if ! applypatch -c EMMC:/dev/block/platform/msm_sdcc.1/by-name/recovery:9658368:01483faa796d46ca7b7de1c2df04181365f42c56; then
-  applypatch -b /system/etc/recovery-resource.dat EMMC:/dev/block/platform/msm_sdcc.1/by-name/boot:8796160:89f1a48949eb0a54496d87168c7f6c2b5c51968c EMMC:/dev/block/platform/msm_sdcc.1/by-name/recovery 01483faa796d46ca7b7de1c2df04181365f42c56 9658368 89f1a48949eb0a54496d87168c7f6c2b5c51968c:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
+if ! applypatch -c EMMC:/dev/block/platform/msm_sdcc.1/by-name/recovery:10520576:5f5d5cf9412973a834f6cb4b470a0c3e0cd97e1c; then
+  applypatch EMMC:/dev/block/platform/msm_sdcc.1/by-name/boot:9252864:ab3511a236493ff4cc704e3813f017324a533a17 EMMC:/dev/block/platform/msm_sdcc.1/by-name/recovery 5f5d5cf9412973a834f6cb4b470a0c3e0cd97e1c 10520576 ab3511a236493ff4cc704e3813f017324a533a17:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
 else
   log -t recovery "Recovery image already installed"
 fi
