9747ef15203fe047221e64cd12ad00d9f67f2680
SoUnd001 <sound0020@gmail.com>
2017-01-14 13:27:40 +0200

    Fixes

--- init/version.c
+++ init/version.c
@@ -44,5 +44,5 @@ const char linux_banner[] =
 
 const char linux_proc_banner[] =
 	"%s version %s"
-	"("By SoUnd001@XDA Developers")"
+	" ("By SoUnd001@XDA Developers")"
 	" (" LINUX_COMPILER ") %s\n";
