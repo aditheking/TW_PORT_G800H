d971558c9a06a35db72de2d6fde8be0c4dc3fae2
aditheking <aupreti72@gmail.com>
2017-01-15 03:59:06 +0000

    Revert "Merge agin indian firmware"
    This reverts commit 01a4a86b33148278cf7dc3ac9fa8d1561bad7e76.

--- system/bin/install-recovery.sh
+++ system/bin/install-recovery.sh
@@ -1,6 +1,1 @@
 #!/system/bin/sh
-if ! applypatch -c EMMC:/dev/block/platform/msm_sdcc.1/by-name/recovery:10520576:5f5d5cf9412973a834f6cb4b470a0c3e0cd97e1c; then
-  applypatch EMMC:/dev/block/platform/msm_sdcc.1/by-name/boot:9252864:ab3511a236493ff4cc704e3813f017324a533a17 EMMC:/dev/block/platform/msm_sdcc.1/by-name/recovery 5f5d5cf9412973a834f6cb4b470a0c3e0cd97e1c 10520576 ab3511a236493ff4cc704e3813f017324a533a17:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
-else
-  log -t recovery "Recovery image already installed"
-fi
